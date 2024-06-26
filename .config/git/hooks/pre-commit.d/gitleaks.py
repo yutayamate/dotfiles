198d1890760723ba24e5358b5c78cf193beb4a1c
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2024-06-26 18:50:47 +0900

    Add Git pre-commit hook

---
+++ .config/git/hooks/pre-commit.d/gitleaks.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# See this page for latest information
+# https://github.com/gitleaks/gitleaks/blob/master/scripts/pre-commit.py
+
+"""Helper script to be used as a pre-commit hook."""
+import os
+import sys
+import subprocess
+
+
+def gitleaksEnabled():
+    """Determine if the pre-commit hook for gitleaks is enabled."""
+    out = subprocess.getoutput("git config --bool hooks.gitleaks")
+    if out == "false":
+        return False
+    return True
+
+
+if gitleaksEnabled():
+    exitCode = os.WEXITSTATUS(os.system('gitleaks protect -v --staged'))
+    if exitCode == 1:
+        print('''Warning: gitleaks has detected sensitive information in your changes.
+To disable the gitleaks precommit hook run the following command:
+
+    git config hooks.gitleaks false
+''')
+        sys.exit(1)
+else:
+    print('gitleaks precommit disabled\
+     (enable with `git config hooks.gitleaks true`)')
