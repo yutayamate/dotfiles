48c6f9f7410d98e03441f7c0bed7c4bc6c73409d
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2025-01-05 21:02:42 +0900

    Add music_info.zsh

--- .zshrc.d/music_info.zsh
+++ .zshrc.d/music_info.zsh
@@ -1,7 +1,15 @@
 function music_info() {
     local _track_name
     music_info_msg=
-    _track_name=$(osascript -e 'try' -e 'tell application "Music" to get name of current track' -e 'end try')
+    _track_name=$(
+        command -v osascript > /dev/null 2>&1 && \
+        osascript \
+            -e 'if application "Music" is running then' \
+            -e 'try' \
+            -e 'tell application "Music" to get name of current track' \
+            -e 'end try' \
+            -e 'end if'
+    )
     if [[ -n $_track_name ]]; then
         music_info_msg="[♫:$_track_name]"
     fi
