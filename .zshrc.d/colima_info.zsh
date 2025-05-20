6e7da5ed50dd6387e7098841be0450c5dc51bb51
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2025-05-20 22:23:00 +0900

    Add docker_info.zsh

--- .zshrc.d/colima_info.zsh
+++
@@ -1,8 +0,0 @@
-function colima_info() {
-    colima_info_msg=
-    if [[ -e $HOME/.colima/default/docker.sock ]]; then
-        colima_info_msg="[colima:running]"
-    fi
-}
-
-add-zsh-hook precmd colima_info
