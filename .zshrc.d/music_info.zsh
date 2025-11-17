4c71e9defba973df91cb811c797b34ae15f1e038
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2025-11-17 15:59:59 +0900

    Update music_info.zsh

--- .zshrc.d/music_info.zsh
+++ .zshrc.d/music_info.zsh
@@ -1,23 +1,16 @@
 function music_info() {
     local _track_name
     music_info_msg=
-    _track_name=$(
-        command -v osascript > /dev/null 2>&1 && \
-        osascript -e '''
-            if application "Music" is running then
+    if [[ $(pgrep -f Music.app) ]]; then
+        _track_name=$(
+            command -v osascript > /dev/null 2>&1 && \
+            osascript -e '''
                 try
                     tell application "Music" to get name of current track
                 end try
-            end if
-        '''
-        # osascript -l JavaScript -e '''
-        #     var app = Application("Music");
-        #     if (app.running()) {
-        #         var track = app.currentTrack()
-        #         console.log(track.name())
-        #     }
-        # ''' 2>&1
-    )
+            '''
+        )
+    fi
     if [[ -n $_track_name ]]; then
         music_info_msg="[♫:$_track_name]"
     fi
