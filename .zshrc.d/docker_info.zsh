b20dbb8edb4284dae6a9027f684e584ecf71c93c
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2025-12-14 00:54:25 +0900

    Remove unnecessary lines

--- .zshrc.d/docker_info.zsh
+++ .zshrc.d/docker_info.zsh
@@ -1,11 +1,5 @@
 function docker_info() {
     docker_info_msg=
-    if [[ -e $HOME/.colima/default/docker.sock ]]; then
-        docker_info_msg+="[colima:running]"
-    fi
-    if [[ -e $HOME/.orbstack/run/docker.sock ]]; then
-        docker_info_msg+="[orbstack:running]"
-    fi
     if [[ $(pgrep -f podman-machine) ]]; then
         docker_info_msg+="[podman:running]"
     fi
