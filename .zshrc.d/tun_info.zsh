3c78c41e817bac34658ec1fe31bad3177435d5cc
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2026-03-23 21:02:01 +0900

    Update tun_info.zsh

--- .zshrc.d/tun_info.zsh
+++ .zshrc.d/tun_info.zsh
@@ -1,7 +1,7 @@
 function tun_info() {
     local _addr_num
     tun_info_msg=
-    if ifconfig | grep -A2 -E '^(u?tun|wg)[0-9]' | grep -qE 'inet [0-9]+\.'; then
+    if ifconfig | grep -A3 -E '^(u?tun|wg)[0-9]' | grep -qE 'inet [0-9]+\.'; then
         tun_info_msg="[tun:enabled]"
     fi
 }
