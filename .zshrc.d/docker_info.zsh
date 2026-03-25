90a762a2692629df3d6820904619c79dfcec5f39
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2026-03-25 21:58:38 +0900

    Rename docker_info.zsh to container_info.zsh

--- .zshrc.d/docker_info.zsh
+++
@@ -1,8 +0,0 @@
-function docker_info() {
-    docker_info_msg=
-    if pgrep -f podman-machine >/dev/null 2>&1; then
-        docker_info_msg+="[podman:running]"
-    fi
-}
-
-add-zsh-hook precmd docker_info
