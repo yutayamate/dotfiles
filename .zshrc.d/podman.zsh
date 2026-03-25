c38d418dcd5f7f150d2a2dc21dda779f5280c942
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2026-03-25 22:01:07 +0900

    Add podman.zsh

---
+++ .zshrc.d/podman.zsh
@@ -0,0 +1,7 @@
+function podman() {
+    if pgrep -f podman-machine >/dev/null 2>&1; then
+        export DOCKER_HOST=unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}') DOCKER_BUILDKIT=0 COMPOSE_DOCKER_CLI_BUILD=0
+    fi
+}
+
+add-zsh-hook precmd podman
