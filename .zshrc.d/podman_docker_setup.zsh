5110475682a28f1c6c866bd30fdb4094fcb7cc44
Yuta Yamate <11874627+yutayamate@users.noreply.github.com>
2026-03-25 22:04:17 +0900

    Rename podman.zsh to podman_docker_setup.zsh

--- .zshrc.d/podman_docker_setup.zsh
+++ .zshrc.d/podman_docker_setup.zsh
@@ -1,7 +1,7 @@
-function podman() {
+function podman_docker_setup() {
     if pgrep -f podman-machine >/dev/null 2>&1; then
         export DOCKER_HOST=unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}') DOCKER_BUILDKIT=0 COMPOSE_DOCKER_CLI_BUILD=0
     fi
 }
 
-add-zsh-hook precmd podman
+add-zsh-hook precmd podman_docker_setup
